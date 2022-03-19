84bced419347bc4f2985ce3f82e82bcfe1a6506c
ldavid03 <laszlo.david23@gmail.com>
2022-03-19 16:58:34 +0100

    asdasd asdasdasdas

--- css/script.js
+++
@@ -1,7 +0,0 @@
-var sticky = document.getElementById("navbar").offsetTop;
-window.onscroll  = function myFunction() {
-    console.log(sticky);
-    if (window.scrollY > sticky) navbar.classList.add("sticky");
-    else navbar.classList.remove("sticky");
-
-}
